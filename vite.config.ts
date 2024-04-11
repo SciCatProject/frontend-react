9afd393084727e020ec7ac573eb3da1b10d65f62
junjiequan <a331998513@gmail.com>
2024-04-11 11:37:52 +0200

    changed application running port to 8888

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8888,
+    open: true,
+  },
   plugins: [react()],
-})
+});
