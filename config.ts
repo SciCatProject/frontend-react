0879d8caced5c6ce619e9b199967643c511eb2d5
junjiequan <a331998513@gmail.com>
2024-05-01 11:02:16 +0200

    udpate config

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://scicat.swap.ess.eu/api/v3/';
-export const BASE_URL = 'http://localhost:3000/api/v3/';
+export const BASE_URL = 'https://scicat.swap.ess.eu/api/v3/';
+// export const BASE_URL = 'http://localhost:3000/api/v3/';
